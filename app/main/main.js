3b095d7be7669001f3b54f001e8dce71eda69a40
streetturtle <streetturtle@gmail.com>
2016-01-31 17:12:37 +0100

    audio added

--- app/main/main.js
+++ app/main/main.js
@@ -2,46 +2,71 @@
 
 angular.module('myApp.main', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'app/main/main.html',
     controller: 'MainCtrl'
   });
 }])
 
-.controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+.controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 
-  $scope.showSettings=false;
+  $scope.showSettings = false;
 
-  $scope.show=false;
-  $scope.lower=0;
-  $scope.upper=10;
+  $scope.show = false;
+  $scope.lower = 0;
+  $scope.upper = 10;
 
-  $scope.langOptions = [{ name: 'English', code: 'en' },
-                        { name: 'French', code: 'fr' },
-                        { name: 'Spanish', code: 'sp' },
-                        { name: 'Portuguese', code: 'pt' }];
+  $scope.langOptions = [{
+      name: 'English',
+      code: 'en'
+    },
+    {
+      name: 'French',
+      code: 'fr'
+    },
+    {
+      name: 'Spanish',
+      code: 'sp'
+    },
+    {
+      name: 'Portuguese',
+      code: 'pt'
+    }];
 
   $scope.language = $scope.langOptions[0].code;
 
-  $scope.generateWord = function(){
-    $scope.result = writtenNumber($scope.number, { lang: $scope.language });
+  $scope.generateWord = function () {
+    $scope.result = writtenNumber($scope.number, {
+      lang: $scope.language
+    });
   }
 
-  $scope.refresh = function(){
+  $scope.play = function () {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = $scope.number.toString();
+    msg.lang = $scope.language;
+    window.speechSynthesis.speak(msg);
+  }
+
+  //  $scope.play();
+
+
+  $scope.refresh = function () {
     $scope.number = Math.floor(Math.random() * $scope.upper) + $scope.lower;
     $scope.generateWord();
-    $scope.inputString='';
-    $scope.show=false;
+    $scope.inputString = '';
+    $scope.show = false;
+    $scope.play();
   }
 
   $scope.refresh();
 
-  $scope.check = function(){
-    if ($scope.inputString == $scope.result){
-      $timeout(function(){
+  $scope.check = function () {
+    if ($scope.inputString == $scope.result) {
+      $timeout(function () {
         return $scope.refresh();
-      },500)
+      }, 500)
     }
   }
 }]);
